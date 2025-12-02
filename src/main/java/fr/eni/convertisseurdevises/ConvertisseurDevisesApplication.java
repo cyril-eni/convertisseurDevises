5820a330cd8c0acaa1456755a3dd84440856e258
Cyril Mace <cmace@campus-eni.fr>
2025-12-02 10:49:42 +0100

    setup

---
+++ src/main/java/fr/eni/convertisseurdevises/ConvertisseurDevisesApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.convertisseurdevises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvertisseurDevisesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConvertisseurDevisesApplication.class, args);
+    }
+
+}
